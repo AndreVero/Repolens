d965859983f9763efec17a67191a09df077bf8ef
Andrii <39765531+AndreVero@users.noreply.github.com>
2026-05-15 18:46:18 +0100

    Fix issues with bob v2

--- app/src/main/java/com/vero/repolens/ui/screens/SearchScreen.kt
+++ app/src/main/java/com/vero/repolens/ui/screens/SearchScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.vero.repolens.data.models.SearchFilter
 import com.vero.repolens.data.models.SearchableItem
 import com.vero.repolens.data.models.SearchItemType
 import com.vero.repolens.ui.components.LoadingState
